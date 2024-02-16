1dd341ea2c6b3f3300daae2c5b1394c89a673b8f
mitrajani506 <rmit9542@gmail.com>
2024-02-16 12:15:11 +0530

    chang

--- src/App.js
+++ src/App.js
@@ -5,68 +5,66 @@ import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 
 function App() {
+  const url = "http://localhost:3001/posts";
+  const [data, setdata] = useState([]);
 
-  const url = "http://localhost:3001/posts"
-  const [data,setdata]= useState([])
+  useEffect(() => {
+    Fdata();
+  }, []);
 
-  useEffect(()=>{
-    Fdata()
-  },[])
-
-  const Fdata = ()=>{
-    axios.get(url).then((res)=>{
+  const Fdata = () => {
+    axios.get(url).then((res) => {
       console.log(res.data);
-      setdata(res.data)
-    })
-  }
+      setdata(res.data);
+    });
+  };
 
   // post
 
-  const [Udata,setUdata] = useState([])
+  const [Udata, setUdata] = useState([]);
 
-  const chang = (e) =>{
-    setUdata({...Udata,[e.target.name]:e.target.value})
-  }
+  const chang = (e) => {
+    setUdata({ ...Udata, [e.target.name]: e.target.value });
+  };
 
-  const Submit = () =>{
-    axios.post("http://localhost:3001/posts",Udata).then((res)=>{
+  const Submit = () => {
+    axios.post("http://localhost:3001/posts", Udata).then((res) => {
       console.log(res);
-      Fdata()
-    })
-  }
+      Fdata();
+    });
+  };
 
   // delete
 
-  const DeleteData = (id) =>{
-    axios.delete(`http://localhost:3001/posts/${id}`).then((res)=>{
-      const delet = data.filter((e)=>e.id !== id)
-      setdata(delet)
-    })
-  }
+  const DeleteData = (id) => {
+    axios.delete(`http://localhost:3001/posts/${id}`).then((res) => {
+      const delet = data.filter((e) => e.id !== id);
+      setdata(delet);
+    });
+  };
 
   // edit
 
-  const [Edata,setEdata]= useState(null)
+  const [Edata, setEdata] = useState(null);
 
-  const Editdata = (index) =>{
-    const edit = data[index]
-    setEdata(edit)
-  }
+  const Editdata = (index) => {
+    const edit = data[index];
+    setEdata(edit);
+  };
 
-  const onchangeEdit = (e)=>{
-    setEdata({...Edata,[e.target.name]:e.target.value})
-  }
+  const onchangeEdit = (e) => {
+    setEdata({ ...Edata, [e.target.name]: e.target.value });
+  };
 
-  const edithandal = (id) =>{
-    axios.put(`http://localhost:3001/posts/${id}`,Edata).then((res)=>{
-      Fdata()
-      setEdata(null)
-    })
-  }
+  const edithandal = (id) => {
+    axios.put(`http://localhost:3001/posts/${id}`, Edata).then((res) => {
+      Fdata();
+      setEdata(null);
+    });
+  };
 
   return (
     <>
-
       <input type="text" name="firstName" onChange={chang} />
       <input type="text" name="lastName" onChange={chang} />
       <button onClick={Submit}>Submit</button>
@@ -75,27 +73,40 @@ function App() {
 
       {Edata && (
         <>
-          <input type="text" name="firstName" value={Edata.firstName} onChange={onchangeEdit} />
-          <input type="text" name="lastName" value={Edata.lastName} onChange={onchangeEdit} />
-          <button onClick={()=>edithandal(Edata.id)}>Edit</button>
+          <input
+            type="text"
+            name="firstName"
+            value={Edata.firstName}
+            onChange={onchangeEdit}
+          />
+          <input
+            type="text"
+            name="lastName"
+            value={Edata.lastName}
+            onChange={onchangeEdit}
+          />
+          <button onClick={() => edithandal(Edata.id)}>Edit</button>
         </>
       )}
 
-        {/* sow data */}
+      {/* sow data */}
 
-      {
-        data.map((i,index)=>{
-          return(
-            <div key={index}>
-              <>{i.firstName}</>
-              <>{i.lastName}</>
-              <button onClick={()=>DeleteData(i.id)}>Delete</button>
-              <button onClick={()=>{Editdata(index)}}>Edit</button>
-            </div>
-          )
-        })
-      }
-
+      {data.map((i, index) => {
+        return (
+          <div key={index}>
+            <>{i.firstName}</>
+            <>{i.lastName}</>
+            <button onClick={() => DeleteData(i.id)}>Delete</button>
+            <button
+              onClick={() => {
+                Editdata(index);
+              }}
+            >
+              Edit
+            </button>
+          </div>
+        );
+      })}
     </>
   );
 }
